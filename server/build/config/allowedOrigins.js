1bc853c04a29e7bf123c00cb86b58bd218d967c7
Pasquale Milone Netto <pasqualenetto@yahoo.com.br>
2022-09-18 02:38:54 -0300

    cors....

--- server/build/config/allowedOrigins.js
+++ server/build/config/allowedOrigins.js
@@ -4,6 +4,7 @@ const allowedOrigins = [
     'https://www.meusite.com',
     'http://127.0.0.1:5173/',
     'http://localhost:3000',
-    'http://localhost:3333'
+    'http://localhost:3333',
+    'https://nlwesports-nine.vercel.app/'
 ];
 exports.default = allowedOrigins;
