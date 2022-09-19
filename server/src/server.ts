911fddd1335be5f5e5a1fde3fcd3c9d8b7efd465
Pasquale Milone Netto <pasqualenetto@yahoo.com.br>
2022-09-19 20:34:34 -0300

    add console.log on port listen

--- server/src/server.ts
+++ server/src/server.ts
@@ -105,4 +105,4 @@ app.get('/ads/:id/discord', async (request, response)=>{
     })
 }) 
    
-app.listen(process.env.PORT || 3333)
+app.listen(process.env.PORT || 3333, ()=> console.log('HTTP Server está rodando!'))
