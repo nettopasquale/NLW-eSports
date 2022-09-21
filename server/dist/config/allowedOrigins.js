96082007775a1868b0a58b0afa8ab1d3132c37ff
Pasquale Milone Netto <pasqualenetto@yahoo.com.br>
2022-09-20 22:59:47 -0300

    teste dist

---
+++ server/dist/config/allowedOrigins.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const allowedOrigins = [
+    'https://www.meusite.com',
+    'http://127.0.0.1:5173/',
+    'http://localhost:3000',
+    'http://localhost:3333',
+    'http://localhost:3333/games',
+    'https://nlwesports-nine.vercel.app/'
+];
+exports.default = allowedOrigins;
